c25334fa908cfb92096e93440e72fe45797524d3
AmeerSohail98 <ameersohailsheik@gmail.com>
2025-11-04 19:05:08 +0530

    Updated by sohail on 04-11-2025 for functions and packages

---
+++ Day-4/practice/advn_calc.py
@@ -0,0 +1,5 @@
+import calc as bc
+
+bc
+# bc.mul()  #for the global variable input
+
---
+++ Day-4/practice/calc.py
@@ -0,0 +1,30 @@
+# a= int(input())
+# b= int(input())
+#-----------------OR----------------------------#
+def addition(a,b):
+    ad= (a+b)
+    return ad
+
+def sub(a,b):
+    s =(a-b)
+    return s
+
+def mul(a,b):
+    m= (a*b)
+    return m
+
+def div(a,b):
+    d = (a//b)
+    return d
+
+# if __name__ == "__calc__":
+# print(addition())
+# print(sub())
+# print(mul())
+# print(div())
+
+#-----------------OR----------------------------#
+print(addition(2,3))
+print(sub(4,5))
+print(mul(3,4))
+print(div(2,6))
