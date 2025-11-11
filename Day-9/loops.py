0e90a67ea9abae95d024f9b6d5b232ec87b82f08
AmeerSohail98 <ameersohailsheik@gmail.com>
2025-11-11 18:20:30 +0530

    Updated by Ameer sohail for loops concept on 11-11-2025

---
+++ Day-9/loops.py
@@ -0,0 +1,26 @@
+#For loop
+fruits = ['apple','Orange', 'banana']
+for fruit in fruits:
+    print(fruit)
+
+#While loop
+count =0
+while count < 5:
+    print(count)
+    count+=1
+
+#break Statement for loop
+
+numbers = [1, 2, 3, 4, 5]
+for number in numbers:
+    if number ==3:
+        break
+    print(number)
+
+#break statement while loop
+count = 0
+while count <=10:
+    if count == 7:
+        break
+    count+=1
+    print(count)
